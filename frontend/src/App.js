b95f25d48525171ce071995465895bb8c3a5e13e
Riley Slayden <slaydenriley@gmail.com>
2021-01-22 14:10:35 -0600

    Building basic home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Home from './components/Home.js';
 
 
 class App extends Component {
   render() {
     return (
-      <div><h1>Riley Slayden Portfolio</h1></div>
+      <Home/>
     )
   };
 };
