fa20dca4626055097695407c378281674d4c581a
Riley Slayden <slaydenriley@gmail.com>
2021-01-22 14:36:05 -0600

    Setting up store in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+// Set Up Store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
