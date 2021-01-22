add5cf865d8d21d29b55d1959a9418040c2945c6
Riley Slayden <slaydenriley@gmail.com>
2021-01-22 10:19:24 -0600

    Began configuring post and project controllers

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+  end
+  
 end
