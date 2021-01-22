3bdddca2e20c1a7091a33d86ff5f940fbe634d97
Riley Slayden <slaydenriley@gmail.com>
2021-01-22 13:32:33 -0600

    Built project_comments controller and serializer

--- backend/app/controllers/project_comments_controller.rb
+++ backend/app/controllers/project_comments_controller.rb
@@ -1,2 +1,54 @@
 class ProjectCommentsController < ApplicationController
+  def create
+    project_comment = ProjectComment.new(project_comments_params)
+    if project_comment.save
+      render json: ProjectCommentsSerializer.new(project_comment).to_serialized_json
+      session[:user_id] = project_comment.id
+    else
+      payload = {
+        error: "Something went wrong. Please try again.",
+        status: 400
+      }
+      render :json => payload, :status => :bad_request
+    end
+  end
+
+  def show
+    project_comment = ProjectComment.find_by(id: params[:id])
+    render json: ProjectCommentsSerializer.new(project_comment).to_serialized_json
+  end
+
+  def index
+    users = ProjectComment.all
+    render json: ProjectCommentsSerializer.new(users).to_serialized_json
+  end
+
+  def edit
+    project_comment = ProjectComment.find_by(id: project_comments_params[:id])
+    project_comment.update(project_comments_params)
+    if project_comment.save
+      render json: ProjectCommentsSerializer.new(project_comment).to_serialized_json
+    else
+      payload = {
+        error: "Something went wrong. Please try again.",
+        status: 400
+      }
+      render :json => payload, :status => :bad_request
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def project_comments_params
+    params.require(:project_comment).permit(
+      :name,
+      :link,
+      :description,
+      :user_id
+    )
+  end
+
 end
