028320c09363357d3bddb29055341b3ef662c286
Riley Slayden <slaydenriley@gmail.com>
2021-01-25 10:59:29 -0600

    Created a sessions controller and login helper methods

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :projects
   resources :users
+
+  post '/login',    to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
