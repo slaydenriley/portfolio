b05df1a6565d1dd8fab6ed6813a8bdc7abe490ed
Riley Slayden <slaydenriley@gmail.com>
2021-01-25 14:48:06 -0600

    Basic styling on account and posts pages

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -4,7 +4,16 @@ const Account = (props) => {
 
   return (
     <div>
-      {props.account.map(user => <div key={user.id}><em>Logged in: {user.name}</em></div>)}
+      <h1>My Account</h1>
+
+      {props.account.map(user =>
+        <div key={user.id}>
+          <em>Logged in: {user.name}</em>
+          <h2>Description</h2>
+          <p>{user.description}</p>
+          <h2>Email</h2>
+          <a href={"mailto:" + user.email}>{user.email}</a>
+        </div>)}
     </div>
 
   )
--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Posts = props => {
   return (
     <div>
+      <h1>Blog Posts</h1>
       {props.posts.map(post =>
         <div key={post.id}>
           <h2>Title: {post.title}</h2>
