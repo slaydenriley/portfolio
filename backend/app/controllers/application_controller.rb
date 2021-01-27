3174b4013c650f0427201120fccbcde5ea0c821e
Riley Slayden <slaydenriley@gmail.com>
2021-01-27 14:45:40 -0600

    Got login to work

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!, :set_user
 
   def login!
@@ -24,5 +25,5 @@ class ApplicationController < ActionController::Base
   def set_user
     @user = User.find_by(id: session[:user_id])
   end
-  
+
 end
