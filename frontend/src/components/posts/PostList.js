42fbd979a42f5eb616e258f3c277c0c5236999b5
Riley Slayden <slaydenriley@gmail.com>
2021-01-29 16:12:51 -0600

    Blog posts now show max description character length of 250

--- frontend/src/components/posts/PostList.js
+++ frontend/src/components/posts/PostList.js
@@ -6,13 +6,12 @@ class PostList extends React.Component {
   render() {
     return (
       <div className="posts">
-        <h1>Blog Posts</h1>
+        <h1><u>Blog Posts</u></h1>
         {this.props.posts.map(post =>
-          <div key={post.id}>
-            <h2>Title: {post.title}</h2>
-            <em>Comments: {post.post_comments.length}</em><br/>
-            <Link key={post.id} to={`/posts/${post.id}`}>Read More...</Link>
-
+          <div key={post.id} className="post-list">
+            <h1><em>{post.title}</em></h1>
+            <p>{post.content.substring(0, 250)}... <em><Link key={post.id} to={`/posts/${post.id}`}>Read More</Link></em></p>
+            <em>Comments: {post.post_comments.length}</em>
           </div>)}
       </div>
     );
