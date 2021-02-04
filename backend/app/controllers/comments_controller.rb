c917fa53d54645a873c7b7aa8c3c7ec8d8a95428
Riley Slayden <slaydenriley@gmail.com>
2021-02-04 13:49:16 -0700

    Configured comment controller for authorization

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -15,24 +15,23 @@ class CommentsController < ApplicationController
 
   def destroy
     comment = Comment.find_by(id: comment_params[:id])
-    binding.pry
     post = Post.find_by(id: comment_params[:post_id])
-    if comment.user_id == current_user.id
-      if comment.destroy
-        render json: PostSerializer.new(post).to_serialized_json
+      if comment.user_id == current_user.id
+        if comment.destroy
+          render json: PostSerializer.new(post).to_serialized_json
+        else
+          payload = {
+            error: "Something went wrong. Please try again.",
+            status: 400
+          }
+          render :json => payload, :status => :bad_request
+        end
       else
         payload = {
-          error: "Something went wrong. Please try again.",
+          error: "You don't own this comment!",
           status: 400
         }
-        render :json => payload, :status => :bad_request
       end
-    else
-      payload = {
-        error: "You don't own this comment!",
-        status: 400
-      }
-    end
   end
 
   private
--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
   end
 
   def show
+  
     post = Post.find_by(id: params[:id])
     render json: PostSerializer.new(post).to_serialized_json
   end
