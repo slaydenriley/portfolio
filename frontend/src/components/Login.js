323ef24f9386c6ae99020e50aafe61ca23aece7a
Riley Slayden <slaydenriley@gmail.com>
2021-02-04 09:17:08 -0700

    Moved logic out of login component and to loginContainer

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,42 +1,17 @@
 import React from 'react';
-import {connect} from 'react-redux'
-import loginUser from '../actions/loginUser'
-import {Redirect} from 'react-router-dom'
 
-class Login extends React.Component {
+const Login = () => {
 
-  state = {
-    email: '',
-    password: ''
-  }
-
-  handleChange = (event) =>  {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault()
-    let formData = {user: this.state}
-    this.props.loginUser(formData)
-  };
-
-  render() {
     return (
       <div className="login">
         <h1>Login</h1>
-        <form onSubmit={this.handleSubmit}>
-
-          <input type='text' placeholder='Email' value={this.props.email} name="email" onChange={this.handleChange}/><br/>
-
-          <input type='password' placeholder='Password' value={this.props.password} name="password" onChange={this.handleChange}/><br/>
+        <form>
+          <input type='text' placeholder='Email' name="email"/><br/>
+          <input type='password' placeholder='Password' name="password"/><br/>
           <input className="submit" type="submit"/>
         </form>
       </div>
     )
-  }
 }
 
-
-export default connect(null, {loginUser})(Login)
+export default Login
--- frontend/src/containers/authorized/loginContainer.js
+++ frontend/src/containers/authorized/loginContainer.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import Login from '../../components/Login'
+import {connect} from 'react-redux'
+import loginUser from '../../actions/loginUser'
+import {Redirect} from 'react-router-dom'
 
 class LoginContainer extends React.Component {
 
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = (event) =>  {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    let formData = {user: this.state}
+    this.props.loginUser(formData)
+  };
+
   render() {
     return (
-      <div>
+      <div onChange={this.handleChange} onSubmit={this.handleSubmit}>
         <Login />
       </div>
     )
   }
 }
 
-export default LoginContainer
+export default connect(null, {loginUser})(LoginContainer)
