22efff2532e26b6559da54b14918febb3cd21236
Riley Slayden <slaydenriley@gmail.com>
2021-02-04 10:12:22 -0700

    Removed post and project boolean from post, changed to category string

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_200309) do
+ActiveRecord::Schema.define(version: 2021_02_04_171142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,7 @@ ActiveRecord::Schema.define(version: 2021_02_03_200309) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "project"
-    t.boolean "post"
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
