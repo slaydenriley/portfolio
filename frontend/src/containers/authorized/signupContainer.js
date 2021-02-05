eebf7fbacc73de6281033245f92f79b8c2ec7c2c
Riley Slayden <slaydenriley@gmail.com>
2021-02-05 09:26:42 -0700

    Redirect user after signup

--- frontend/src/containers/authorized/signupContainer.js
+++ frontend/src/containers/authorized/signupContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import signupUser from '../../actions/signupUser'
 import SignupForm from '../../components/Signup'
+import {Redirect} from 'react-router-dom'
 
 class SignupContainer extends React.Component {
 
@@ -24,13 +25,30 @@ class SignupContainer extends React.Component {
     this.props.signupUser(formData)
   }
 
+  handleSignup = () => {
+    if (this.props.logged_in === false) {
+      return(
+        <div onChange={this.handleChange} onSubmit={this.handleSubmit}>
+          <SignupForm />
+        </div>)
+    } else {
+      return <Redirect to="/dashboard" />
+    }
+  }
+
   render() {
     return (
-      <div onChange={this.handleChange} onSubmit={this.handleSubmit}>
-        <SignupForm />
+      <div>
+        {this.handleSignup()}
       </div>
     )
   }
 }
 
-export default connect(null, {signupUser})(SignupContainer)
+const mapStateToProps = (state) => {
+  return {
+    logged_in: state.account.logged_in
+  }
+}
+
+export default connect(mapStateToProps, {signupUser})(SignupContainer)
