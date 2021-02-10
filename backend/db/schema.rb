b20ab1d01098ae683b4599c991af932978c59b36
Riley Slayden <slaydenriley@gmail.com>
2021-02-10 16:22:12 -0700

    Created tags table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_223623) do
+ActiveRecord::Schema.define(version: 2021_02_10_232052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_02_10_223623) do
     t.string "category"
     t.string "image_link"
     t.string "tag"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
