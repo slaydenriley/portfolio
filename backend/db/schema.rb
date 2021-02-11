0592c03668feb9f69b72f6c812ddb362139fcdb1
Riley Slayden <slaydenriley@gmail.com>
2021-02-10 21:13:27 -0700

    Removed tag column from post table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_040911) do
+ActiveRecord::Schema.define(version: 2021_02_11_041247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_02_11_040911) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
     t.string "image_link"
-    t.string "tag"
   end
 
   create_table "posts_tags", id: false, force: :cascade do |t|
